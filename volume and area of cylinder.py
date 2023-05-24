ae15478a1f9ad173a58bad924d567ca9f3dab5b5
Akalya-MS <133938616+Akalya-MS@users.noreply.github.com>
2023-05-24 22:24:16 +0530

    Add files via upload

---
+++ volume and area of cylinder.py
@@ -0,0 +1,8 @@
+r=int(input("enter radius:"))
+h=int(input("enter height:"))
+v=3.14*r*r*h
+a=((2*3.14*r)*h)+((3.14*r**2)*2)
+print(v,"is volume of cylinder")
+print(a,"is area of cylinder")
+
+    
